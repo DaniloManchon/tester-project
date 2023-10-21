ab6ab9a5eac38a96d2082c62549830014ee44737
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2023-10-21 12:53:44 -0300

    feat: Simplisy solution

--- test/src/main/kotlin/com/example/test/service/TesteService.kt
+++ test/src/main/kotlin/com/example/test/service/TesteService.kt
@@ -10,36 +10,10 @@ import org.springframework.stereotype.Service
 
 @Service
 class TesteService {
-
-    fun jsonMapper(event: String): MutableMap<String, Any?> {
-        val mapAdapter = Gson().getAdapter(object: TypeToken<MutableMap<String, Any?>>() {})
-        val mappedEvent: MutableMap<String, Any?> = mapAdapter.fromJson(event)
-        return mappedEvent
-    }
-
-    fun jsonFilter(mappedEvent:  MutableMap<String, Any?>): MutableMap<String, Any?> {
-        mappedEvent.remove("nome")
-        mappedEvent.remove("idade")
-        return mappedEvent
-    }
-
-    fun mapperToObject(mappedEvent:  MutableMap<String, Any?>): TestModel{
-        return TestModel(
-            mappedEvent.getValue("nome").toString(),
-            Integer.parseInt(mappedEvent.getValue("idade").toString()),
-                jsonFilter(mappedEvent)
-        )
-    }
-
-   fun dataValidation(event: String): TestModel {
-       val dataMapeadaFull = jsonMapper(event)
-       val objetoMapeado = mapperToObject(dataMapeadaFull)
-
-       if(objetoMapeado.idade >= 18){
-           objetoMapeado.nome = "Danilo"
+   fun dataValidation(event: TestModel): TestModel {
+       if(event.idade >= 18){
+           event.nome = "Danilo"
        }
-
-       return objetoMapeado
+       return event
    }
-
 }
